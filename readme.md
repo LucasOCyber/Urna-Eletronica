684ebb14bf1dc734924466566ebc1404b863f093
Lucas Silva <lucascyber1999@gmail.com>
2023-09-27 13:41:03 -0300

    Urna

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Urna-Eletronica
+ Projeto: Urna Eletrônica
+
+Desenvolvido com HTML5, CSS e JS.
+
+Vote em: 
+
+VEREADOR
+    nome: fulano de tal
+    numero: '38111'
+    partido: ABC
+
+    nome: beltrano da silva
+    numero: 77222
+    partido: DEFG
+
+
+PREFEITO
+    nome: Ciclano
+    numero: 99
+    partido: ABC
+    vice: Cic
+
+    nome: zulano
+    numero: 84
+    partido: QWERT
+    vice: Zul
---
+++ script/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero:'38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [ {url:'../img/pessoa3.jpeg', legenda: 'vereador', small: false}
+                ]
+            },
+            {
+                numero:'77222',
+                nome: 'beltrano da silva',
+                partido:'DEFG',
+                fotos: [
+                    {url:'../img/pessoa2.jpeg', legenda: 'vereador'},
+                  ]
+
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    {url:'../img/pessoa1.jpeg', legenda: 'Prefeito'},
+                    {url:'../img/pessoa3.jpeg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'zulano',
+                partido: 'QWERT',
+                vice: 'Zul',
+                fotos: [
+                    {url:'../img/pessoa2.jpeg', legenda: 'Prefeito'},
+                    {url:'../img/pessoa3.jpeg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
